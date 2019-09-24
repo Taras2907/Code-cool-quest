1cdfc80d624299f26ea180f0cc28ab7f27488cee
Kendzioor <wojtekadam1@gmail.com>
2019-09-24 13:07:16 +0200

    Add new characters attributes : health points, armor points, damage

--- src/main/java/com/codecool/quest/logic/actors/Player.java
+++ src/main/java/com/codecool/quest/logic/actors/Player.java
@@ -6,6 +6,12 @@ import com.codecool.quest.logic.actors.Actor;
 public class Player extends Actor {
     public Player(Cell cell) {
         super(cell);
+        int damage = 5;
+        int startingHealth = 10;
+        int armor = 1;
+        this.setHealth(startingHealth);
+        this.setDamage(damage);
+        this.setArmor(armor);
     }
 
     public String getTileName() {
