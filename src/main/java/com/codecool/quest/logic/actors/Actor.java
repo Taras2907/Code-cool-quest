84534492bf2d964ce63c70a2c59976096a46f378
Korneliusz Chodnikiewicz <korneliuszchodnikiewicz@gmail.com>
2019-09-24 13:04:06 +0200

    Add restrict movement method to player movement

--- src/main/java/com/codecool/quest/logic/actors/Actor.java
+++ src/main/java/com/codecool/quest/logic/actors/Actor.java
@@ -1,13 +1,14 @@
 package com.codecool.quest.logic.actors;
 
 import com.codecool.quest.logic.Cell;
+import com.codecool.quest.logic.CellType;
 import com.codecool.quest.logic.Drawable;
 
 public abstract class Actor implements Drawable {
     private Cell cell;
     private int health = 10;
     private int damage;
-    private int armor;
+    protected int armor;
 
     public Actor(Cell cell) {
         this.cell = cell;
@@ -16,9 +17,15 @@ public abstract class Actor implements Drawable {
 
     public void move(int dx, int dy) {
         Cell nextCell = cell.getNeighbor(dx, dy);
-        cell.setActor(null);
-        nextCell.setActor(this);
-        cell = nextCell;
+        if (isMovePossible(nextCell)) {
+            cell.setActor(null);
+            nextCell.setActor(this);
+            cell = nextCell;
+        }
+    }
+
+    public boolean isMovePossible(Cell nextCell) {
+        return nextCell.getType().equals(CellType.FLOOR) && (nextCell.getActor() == null);
     }
 
     public int getHealth() {
--- src/main/java/com/codecool/quest/logic/actors/Player.java
+++ src/main/java/com/codecool/quest/logic/actors/Player.java
@@ -4,8 +4,10 @@ import com.codecool.quest.logic.Cell;
 import com.codecool.quest.logic.actors.Actor;
 
 public class Player extends Actor {
+
     public Player(Cell cell) {
         super(cell);
+        this.armor = 2;
     }
 
     public String getTileName() {
