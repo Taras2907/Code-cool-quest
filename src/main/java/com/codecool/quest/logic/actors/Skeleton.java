0b4f401dac966b1a7b8d5c44b1b312b25e06202d
Kendzioor <wojtekadam1@gmail.com>
2019-09-24 13:07:21 +0200

    Add new characters attributes : health points, armor points, damage

--- src/main/java/com/codecool/quest/logic/actors/Skeleton.java
+++ src/main/java/com/codecool/quest/logic/actors/Skeleton.java
@@ -6,6 +6,12 @@ import com.codecool.quest.logic.actors.Actor;
 public class Skeleton extends Actor {
     public Skeleton(Cell cell) {
         super(cell);
+        int damage = 2;
+        int startingHealth = 10;
+        int armor = 1;
+        this.setHealth(startingHealth);
+        this.setDamage(damage);
+        this.setArmor(armor);
     }
 
     @Override
